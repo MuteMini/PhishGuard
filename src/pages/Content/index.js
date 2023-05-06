31ce6a5d343a8e12ad8ff1573411821840728d04
Min <63823931+MuteMini@users.noreply.github.com>
2023-05-06 18:01:24 -0400

    Added rough injecting code test

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,10 @@
-import { printLine } from './modules/print';
+console.log("Content script works!");
+console.log("Must reload extension for modifications to take effect.");
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "scan") {
+        var style = document.createElement("style");
+        style.innerHTML = "html { filter: brightness(0.5); }";
+        document.head.appendChild(style);
+    }
+});
