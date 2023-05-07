307f6425e4b2456c7f04645deaf224fb54b36ed8
Min <63823931+MuteMini@users.noreply.github.com>
2023-05-06 20:04:08 -0400

    Swapped boilerplate

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,10 +1,10 @@
-console.log("Content script works!");
-console.log("Must reload extension for modifications to take effect.");
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "scan") {
-        var style = document.createElement("style");
-        style.innerHTML = "html { filter: brightness(0.5); }";
-        document.head.appendChild(style);
+        const body = document.querySelector("body")
+       
+        const app = document.createElement("div");
+        app.className = "overlay"
+
+        if(body) { body.prepend(app) }
     }
 });
---
+++ src/pages/Popup/Popup.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Stack from "react-bootstrap/Stack";
+import {
+  IoMdSettings,
+  IoMdInformationCircleOutline,
+  IoLogoGithub,
+} from "react-icons/io";
+import "~bootstrap/dist/css/bootstrap.css";
+
+import logo from "@assets/img/logo.png";
+import "@pages/Popup/Popup.css";
+
+const requestScan = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "scan" });
+  });
+};
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <Stack style={{ height: "100%" }} gap={3}>
+        <div className="mt-auto">
+          <Image src={logo} className="App-logo" alt="logo" />
+        </div>
+        <Button
+          className="mt-auto"
+          variant="outline-primary"
+          onClick={() => requestScan()}
+        >
+          Scan Text
+        </Button>
+        <Stack className="mx-auto" direction="horizontal" gap={3}>
+          <Button variant="outline-secondary">
+            <IoMdSettings />
+          </Button>
+          <Button variant="outline-secondary">
+            <IoMdInformationCircleOutline />
+          </Button>
+          <Button variant="outline-secondary">
+            <IoLogoGithub />
+          </Button>
+        </Stack>
+        <div className="lead" style={{ fontSize: "12px" }}>
+          Written by the PhishGuard Team
+        </div>
+      </Stack>
+    </div>
+  );
+};
+
+export default Popup;
