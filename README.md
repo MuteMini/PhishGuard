f9f85c2037f955e3ac94890cbcef136291326457
Min <63823931+MuteMini@users.noreply.github.com>
2023-05-07 04:28:46 -0400

    Added text scanning functionality

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ A google chome extension to help avoid phishing scams
 [Chrome Extension (MV3) Boilerplate](https://github.com/lxieyang/chrome-extension-boilerplate-react) by lxieyang
 [React Bootstrap](https://react-bootstrap.github.io)
 [React Icons](https://react-icons.github.io/react-icons/)
+[withinviewport](https://github.com/patik/within-viewport) by patik
 [Cohere](https://cohere.com)
--- src/pages/Content/Scanner/App.tsx
+++ src/pages/Content/Scanner/App.tsx
@@ -1,17 +1,99 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Stack from "react-bootstrap/Stack";
+import Card from "react-bootstrap/Card";
+
+import { withinViewport } from "withinViewport";
 
 const Scanner = () => {
-  
+  const [pos, setPos] = useState([0, 0]);
+  const [size, setSize] = useState([0, 0]);
+  const [dragDone, setDragDone] = useState(true);
+  const [showOption, setShowOption] = useState(false);
+  const [scanning, setScanning] = useState(false);
 
-  useEffect(() => {
-    console.log("content view loaded");
-  }, []);
+  const handleDown = (e) => {
+    setPos([e.clientX, e.clientY]);
+    setSize([0, 0]);
+    setDragDone(false);
+    setShowOption(false);
+  };
+
+  const handleMove = (e) => {
+    if (!dragDone) {
+      setSize([e.clientX - pos[0], e.clientY - pos[1]]);
+    }
+  };
+
+  const handleUp = () => {
+    if (!(size[0] < 20 && size[1] < 20)) {
+      setShowOption(true);
+    }
+    setDragDone(true);
+  };
+
+  const removeApp = () => {
+    document.body.classList.remove("no-scroll");
+    document.getElementsByClassName("scanner-shadow-ext")[0].remove();
+  };
+
+  const startScan = () => {
+    handleDown({ clientX: 0, clientY: 0 });
+    setScanning(true);
+
+    const viewport = document.querySelector(".viewport");
+    (viewport as HTMLElement).style.margin = "20px";
+    const divs = document.getElementsByTagName("div");
+    let input = "";
+    for (let i = 0; i < divs.length; i++) {
+      if (withinViewport(divs[i], { container: viewport as HTMLElement })) {
+        input += divs[i].textContent;
+      }
+    }
+    input.replace(/\s+/g, " ").trim();
+  };
 
   return (
-    <div 
-      className="overlay"
-      onClick={}
-    ></div>
+    <div
+      className="d-flex flex-column justify-content-start align-items-center"
+      style={{
+        width: "100%",
+        height: "100%",
+        pointerEvents: scanning ? "none" : "unset",
+      }}
+    >
+      <div
+        style={{ width: "100%", height: "100%" }}
+        onMouseDown={handleDown}
+        onMouseMove={handleMove}
+        onMouseUp={handleUp}
+      >
+        <div
+          className="scanning viewport"
+          style={{
+            left: pos[0],
+            top: pos[1],
+            width: size[0],
+            height: size[1],
+          }}
+        ></div>
+      </div>
+      <div className={`popup-card mt-3 ${showOption ? "fade" : ""}`}>
+        <Card.Body>
+          <Card.Subtitle className="m-2">
+            Do you want to scan this?
+          </Card.Subtitle>
+          <Stack className="mx-auto" direction="horizontal" gap={2}>
+            <Button variant="outline-primary" onClick={() => startScan()}>
+              Scan This!
+            </Button>
+            <Button variant="outline-secondary" onClick={() => removeApp()}>
+              Close it.
+            </Button>
+          </Stack>
+        </Card.Body>
+      </div>
+    </div>
   );
 };
 
